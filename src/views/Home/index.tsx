22e3052717860389e673a8b2f3bdad93a363a2a7
fuya <f2956903402@gmail.com>
2023-03-23 21:58:09 +0800

    init

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import { useState,ReactPropTypes } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.scss'
+function Home(props: ReactPropTypes) {
+  return (
+    <div>
+      
+    </div>
+  )
+  
+}
